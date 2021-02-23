3bd71e60efa9f33e4f82a14719c5d9e09fb4621b
ThereseHag <tezzinthecity@gmail.com>
2021-02-23 15:05:49 +0100

    add updates

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const recipesContainer = document.getElementById("recipes-container")
 let userInput = ""
 const searchButton = document.getElementById("search-button")
 const searchForm = document.getElementById("search-form")
-
+const filterCalories = document.getElementById("filter-calories")
+let selectInput = ""
 
 const startingPage = () => { 
   const startURL = `https://api.edamam.com/search?q=cake&app_id=02a54850&app_key=4363b395b71fa21033612dab692612e1&from=0&to=20&calories=391-522`
@@ -31,7 +32,8 @@ fetch(startURL)
 startingPage()
 
 const fetchData = () => { 
-  const API_URL = `https://api.edamam.com/search?q=${userInput}&app_id=02a54850&app_key=4363b395b71fa21033612dab692612e1&from=0&to=20&calories=391-522`
+  const API_URL = `https://api.edamam.com/search?q=${userInput}&app_id=49d10ab5&app_key=b0c519fd0747b629373b6758af019025&from=0&to=20&calories=${selectInput}`
+  console.log(selectInput)
   recipesContainer.innerHTML = ""
   fetch(API_URL)
     .then(response => response.json())
@@ -49,7 +51,7 @@ const fetchData = () => {
             <p>Ingredients: ${element.recipe.ingredientLines}</p>
             <p>Cooking time: ${element.recipe.totalTime} minutes</p>
           </div>
-        ` 
+        `
         });
         
     })
@@ -60,6 +62,12 @@ const fetchData = () => {
       event.preventDefault();
       userInput = searchInput.value;
       console.log(userInput)
-
+      
       fetchData()
+
+    })
+
+    filterCalories.addEventListener("change", (event) => {
+      selectInput = event.target.value;
+      console.log(selectInput)
     })
